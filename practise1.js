c93750c69078090418450cd7de2a9f8f1ab16eda
Saqib Ahmad <you@example.com>
2022-08-26 06:35:54 +0600

    first commit

---
+++ practise1.js
@@ -0,0 +1,12 @@
+const myName = 'Saqib';
+const user = {
+    name: 'Saqib',
+    id: 201,
+    location: 'Dhaka',
+    profession: 'Student'
+};
+console.log(`
+I am ${myName},
+some Des--->
+I am ${user.name}, My Id is-${user.id}, Current Location is at ${user.location}
+`)
---
+++ practise2.js
@@ -0,0 +1,21 @@
+// 1.
+const dividedBy5 = num => num / 5;
+console.log(dividedBy5(59));
+
+// 2.
+const math = (x, y) => {
+    const sum1 = x + 2;
+    const sum2 = y + 2;
+    const product = sum1 * sum2;
+    return product
+}
+console.log(math(2, 5));
+
+// 5.
+const maths = (x, y) => (x + 2) * (y + 2);
+console.log(maths(2, 5))
+
+// 4.
+const product = (x, y, z) => x * y * z;
+console.log(product(2, 4, 5));
+
